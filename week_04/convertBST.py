52febe99975c5f9064147fa485a49c3c7a238000
wangtianyi <email@example.com>
2021-10-31 21:05:36 +0800

    第四周作业

--- week_04/convertBST.py
+++ week_04/convertBST.py
@@ -24,36 +24,3 @@ class Solution:
             convert(root.left)
         convert(root)
         return root
-
-
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-        result = []
-
-        def recursion(node):
-            if not node:
-                return
-            recursion(node.left)
-            result.append(node.val)
-            recursion(node.right)
-        recursion(root)
-        return result
-
-    def levelOrder(self, root: 'Node') -> List[List[int]]:
-
-        result = []
-        queue = []
-
-        if root:
-            queue.append((root, 0))
-
-        while len(queue):
-            node_tuple = queue[0]
-            if len(result) == node_tuple[1]:
-                result.insert(node_tuple[1], [])
-            result[node_tuple[1]].append(node_tuple[0].val)
-            del queue[0]
-            if node_tuple[0].children:
-                for child in node_tuple[0].children:
-                    queue.append((child, node_tuple[1] + 1))
-        return result
-
