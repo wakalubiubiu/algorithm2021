4026f07564e431aac58043e79520bf4877d2575b
wangtianyi <email@example.com>
2021-11-05 21:53:39 +0800

    第一周

---
+++ week_01/maxSlidingWindow.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        queue = []
+        ans = []
+        for i, num in enumerate(nums):
+            if len(queue) > 0 and queue[0] == i - k:
+                del queue[0]
+            while len(queue) > 0 and num >= nums[queue[-1]]:
+                queue.pop()
+            queue.append(i)
+            if i >= k-1:
+                ans.append(nums[queue[0]])
+        return ans
