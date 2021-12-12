f3db1019fc978a2ad1d679bdc29ce9bdcf9db0d3
wangtianyi <email@example.com>
2021-12-12 11:15:32 +0800

    第八周作业

--- week_08/networkDelayTime.py
+++ week_08/networkDelayTime.py
@@ -4,9 +4,10 @@ from typing import List
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        to = [[] for _ in range(n+1)]
-        weight = [[] for _ in range(n+1)]
-        dist = [10**9 for i in range(n+1)]
+        to = [[] for _ in range(n + 1)]
+        weight = [[] for _ in range(n + 1)]
+        dist = [10 ** 9 for i in range(n + 1)]
+        # 先标记每条边可以到大的边和权重，使用出边数组的方式。
         for i in times:
             x = i[0]
             y = i[1]
@@ -15,28 +16,31 @@ class Solution:
             weight[x].append(z)
         dist[k] = 0
         # 标记已经拓展过的点
-        expand = [False for _ in range(n+1)]
-        heap = []
-        heapify(heap)
-        heappush(heap, (0, k))
-        # 此处不要写成for循环的形式，因为这里使用的是惰性删除，这样堆里的元素实际上是多于n个的，这样有部分的元素会被抛弃不适用，如果循环的次数固定为n，那么就会有部分正确的点没有进行拓展更新。
-        while heap:
-            tuple_node = heappop(heap)
-            local = tuple_node[1]
-            temp = tuple_node[0]
-            if expand[local]:
-                continue
+        expand = [False for _ in range(n + 1)]
+        # 循环n次，每次找一个未被标记的最小点，然后更新该点所能到达的所有出边的dist。
+        for i in range(1, n):
+            temp = 10 ** 9
+            local = 0
+            # 找dist中值最小的点切未被标记
+            for j in range(1, n + 1):
+                if dist[j] < temp and expand[j] == False:
+                    temp = dist[j]
+                    local = j
+            # 标记找到的最小的点
             expand[local] = True
-            for j, e in enumerate(to[tuple_node[1]]):
-                if dist[e] > temp + weight[local][j]:
-                    dist[e] = temp + weight[local][j]
-                    heappush(heap, (dist[e], e))
+            # 循环更新找到的最小的点所能到达的所有出边。
+            for j, e in enumerate(to[local]):
+                if dist[e] > weight[local][j] + temp:
+                    dist[e] = weight[local][j] + temp
+            print(dist)
+
         ans = -1
-        for i in range(1, n+1):
+        for i in range(1, n + 1):
             ans = max(dist[i], ans)
-        return ans if ans != 10**9 else -1
+        return ans if ans != 10 ** 9 else -1
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.networkDelayTime([[1,2,1]], 2, 2))
+    print(solution.networkDelayTime([[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2))
+
